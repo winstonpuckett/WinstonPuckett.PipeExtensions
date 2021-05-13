a1f1935efb920748c50aad779eebbea891b1f123
Bobby Puckett <puckett.bobby7@gmail.com>
2021-05-13 11:40:56 -0700

    Update documentation. Rename @operator to func for clarity.

--- WinstonPuckett.PipeExtensions/PipeExtensions.cs
+++ WinstonPuckett.PipeExtensions/PipeExtensions.cs
@@ -6,88 +6,89 @@ namespace WinstonPuckett.PipeExtensions
     public static class PipeExtensions
     {
         /// <summary>
-        /// Take in a function which accepts T returns U.
+        /// Pass T to func and get back U.
         /// </summary>
         /// <typeparam name="T">The type you're operating on.</typeparam>
         /// <typeparam name="U">The type this Pipe returns</typeparam>
         /// <param name="input">The object you're operating on.</param>
-        /// <param name="operator">The function to call which operates on T.</param>
+        /// <param name="func">The function to call which operates on T.</param>
         /// <returns>An object of type U</returns>
-        public static U Pipe<T, U>(this T input, Func<T, U> @operator)
+        public static U Pipe<T, U>(this T input, Func<T, U> func)
         {
-            return @operator(input);
+            return func(input);
         }
 
         /// <summary>
-        /// Take in a function which accepts T returns nothing.
+        /// Pass T to func.
         /// </summary>
         /// <typeparam name="T">The type you're operating on.</typeparam>
         /// <param name="input">The object you're operating on.</param>
-        /// <param name="operator">The function to call which operates on T.</param>
-        public static void Pipe<T>(this T input, Action<T> @operator)
+        /// <param name="func">The function to call which operates on T.</param>
+        public static void Pipe<T>(this T input, Action<T> func)
         {
-            @operator(input);
+            func(input);
         }
 
         /// <summary>
-        /// Take in a Task of T, await T and pass it to @operator, return a Task of U.
+        /// Await T and pass it to func, return a Task of U.
         /// </summary>
         /// <typeparam name="T">The type you're operating on.</typeparam>
         /// <typeparam name="U">The type this Pipe returns</typeparam>
         /// <param name="inputTask">The object you're operating on wrapped in a Task.</param>
-        /// <param name="operator">The function to call which operates on T.</param>
+        /// <param name="func">The function to call which operates on T.</param>
         /// <returns>An object of type U wrapped in a Task</returns>
-        public static async Task<U> PipeAsync<T, U>(this Task<T> inputTask, Func<T, U> @operator)
+        public static async Task<U> PipeAsync<T, U>(this Task<T> inputTask, Func<T, U> func)
         {
-            return @operator(await inputTask);
+            return func(await inputTask);
         }
 
         /// <summary>
-        /// Take in a Task of T, await T and pass it to @operator.
+        /// Await T and pass it to func.
         /// </summary>
         /// <typeparam name="T">The type you're operating on.</typeparam>
         /// <param name="inputTask">The object you're operating on wrapped in a Task.</param>
-        public static async Task PipeAsync<T>(this Task<T> inputTask, Action<T> @operator)
+        /// <param name="func">The function to call which operates on T.</param>
+        public static async Task PipeAsync<T>(this Task<T> inputTask, Action<T> func)
         {
-            @operator(await inputTask);
+            func(await inputTask);
         }
 
         /// <summary>
-        /// Take in a Task of T, await T and pass it to @asyncOperator, return a Task of U.
+        /// Await T and pass it to asyncFunc, return a Task of U.
         /// </summary>
         /// <typeparam name="T">The type you're operating on.</typeparam>
         /// <typeparam name="U">The type this Pipe returns</typeparam>
         /// <param name="inputTask">The object you're operating on wrapped in a Task.</param>
-        /// <param name="asyncOperator">The function to call which operates on T.</param>
+        /// <param name="asyncFunc">The function to call which operates on T.</param>
         /// <returns>An object of type U wrapped in a Task</returns>
-        public static async Task<U> PipeAsync<T, U>(this Task<T> inputTask, Func<T, Task<U>> asyncOperator)
+        public static async Task<U> PipeAsync<T, U>(this Task<T> inputTask, Func<T, Task<U>> asyncFunc)
         {
-            return await asyncOperator(await inputTask);
+            return await asyncFunc(await inputTask);
         }
 
         /// <summary>
-        /// Take in an object of type T, await T and pass it to @asyncOperator, return a Task of U.
+        /// Pass T to asyncFunc, return a Task of U.
         /// </summary>
         /// <typeparam name="T">The type you're operating on.</typeparam>
         /// <typeparam name="U">The type this Pipe returns</typeparam>
         /// <param name="input">The object you're operating on.</param>
-        /// <param name="asyncOperator">The function to call which operates on T.</param>
+        /// <param name="asyncFunc">The function to call which operates on T.</param>
         /// <returns>An object of type U wrapped in a Task</returns>
-        public static async Task<U> PipeAsync<T, U>(this T input, Func<T, Task<U>> asyncOperator)
+        public static async Task<U> PipeAsync<T, U>(this T input, Func<T, Task<U>> asyncFunc)
         {
-            return await asyncOperator(input);
+            return await asyncFunc(input);
         }
 
         /// <summary>
-        /// Take in an object of type T, await T and pass it to @asyncOperator, return a Task of U.
+        /// Await T and pass it to asyncFunc, return a Task of U.
         /// </summary>
         /// <typeparam name="T">The type you're operating on.</typeparam>
         /// <param name="input">The object you're operating on.</param>
-        /// <param name="asyncOperator">The function to call which operates on T.</param>
+        /// <param name="asyncFunc">The function to call which operates on T.</param>
         /// <returns>A Task</returns>
-        public static async Task PipeAsync<T>(this T input, Func<T, Task> asyncOperator)
+        public static async Task PipeAsync<T>(this T input, Func<T, Task> asyncFunc)
         {
-            await asyncOperator(input);
+            await asyncFunc(input);
         }
     }
 }
