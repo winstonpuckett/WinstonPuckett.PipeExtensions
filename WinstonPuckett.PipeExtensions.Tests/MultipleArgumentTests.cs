4ed27abe5a72c57c5d610707c773254a6cb10d36
Bobby Puckett <puckett.bobby7@gmail.com>
2021-05-21 09:02:31 -0700

    Add more "multiple argument" tests

--- WinstonPuckett.PipeExtensions.Tests/MultipleArgumentTests.cs
+++ WinstonPuckett.PipeExtensions.Tests/MultipleArgumentTests.cs
@@ -5,7 +5,7 @@ namespace WinstonPuckett.PipeExtensions.Tests
     public class MultipleArgumentTests
     {
         [Fact]
-        public void CanPassMultipleArguments()
+        public void NamedTuplesWork()
         {
             var person = 
                 (name: "Ingrid", age: 10)
@@ -16,9 +16,51 @@ namespace WinstonPuckett.PipeExtensions.Tests
             Assert.Equal(13, person.age);
         }
 
-        public (string name, int age) AddOneToAge((string name, int age) person)
+        private (string name, int age) AddOneToAge((string name, int age) person)
         {
             return (person.name, person.age + 1);
+        }
+
+        [Fact]
+        public void PassingFromScopedWorks()
+        {
+            int stepsTravelled = 0;
+            int stepsToAddPerCall = 2;
+
+            var totalSteps = 
+                stepsTravelled
+                .Pipe(travelled => AddStepsToTotal(travelled, stepsToAddPerCall))
+                .Pipe(travelled => AddStepsToTotal(travelled, stepsToAddPerCall))
+                .Pipe(travelled => AddStepsToTotal(travelled, stepsToAddPerCall))
+                .Pipe(travelled => AddStepsToTotal(travelled, stepsToAddPerCall))
+                .Pipe(travelled => AddStepsToTotal(travelled, stepsToAddPerCall));
+
+            Assert.Equal(10, totalSteps);
+        }
+
+        private int AddStepsToTotal(int stepsTravelled, int stepsToAddPerCall)
+        {
+            return stepsTravelled + stepsToAddPerCall;
+        }
+
+        [Fact]
+        public void PassingFromScopedWorks_RefactoredAsMonadic()
+        {
+            int stepsTravelled = 0;
+            int stepsToAddPerCall = 2;
+
+            int AddStepsToStepsTravelled(int travelled)
+                => travelled + stepsToAddPerCall;
+
+            var totalSteps =
+                stepsTravelled
+                .Pipe(AddStepsToStepsTravelled)
+                .Pipe(AddStepsToStepsTravelled)
+                .Pipe(AddStepsToStepsTravelled)
+                .Pipe(AddStepsToStepsTravelled)
+                .Pipe(AddStepsToStepsTravelled);
+
+            Assert.Equal(10, totalSteps);
         }
     }
 }
